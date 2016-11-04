9e62310dff6a733ae3cd7006005a46819170ef13
Patrick Luhmann <patrick.luhmann@gmail.com>
2016-11-04 15:54:11 -0700

    Add sample code for getting quote data
    This is just debug code I put in to try, based on stuff I
    found online.
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

--- Couatl2/Program.cs
+++ Couatl2/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;//pjl
+using System.IO;//pjl
 
 namespace Couatl2
 {
@@ -14,6 +16,20 @@ namespace Couatl2
 		[STAThread]
 		static void Main()
 		{
+			System.Diagnostics.Debug.WriteLine("Welcome to Couatl2.");
+
+			// pjl begin test code
+			// based on code from http://csharphelper.com/blog/2015/01/get-stock-prices-from-the-internet-in-c/ 
+			string result;
+			WebClient wc = new WebClient();
+			using (Stream resp = wc.OpenRead("http://download.finance.yahoo.com/d/quotes.csv?s=AAPL+DIS+KO+YHOO&f=sl1d1t1c1"))
+			using (StreamReader sr = new StreamReader(resp))
+			{
+				result = sr.ReadToEnd();
+			}
+			System.Diagnostics.Debug.WriteLine(result);
+			// pjl end test code
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainGui(new Couatl2App()));
